869c84c92bd89e0dfae5f17d4720e43c7419e36e
Prateek Garg <prateekgarg95@gmail.com>
2015-06-27 22:44:24 +0530

    Initial commit

--- app/src/main/java/com/crapp/volleytest/MainActivity.java
+++ app/src/main/java/com/crapp/volleytest/MainActivity.java
@@ -2,19 +2,17 @@ package com.crapp.volleytest;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.HashMap;
-import java.util.Map;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.ImageRequest;
+import com.android.volley.toolbox.NetworkImageView;
 
 
 public class MainActivity extends Activity {
@@ -23,24 +21,32 @@ public class MainActivity extends Activity {
     String URL = "http://quesdesk.hostzi.com/create_user.php";
 
     TextView receivedJSON;
+    ImageView imageView;
+    NetworkImageView networkImageView;
     String text;
 
     ProgressDialog progressDialog;
 
-    @Override
+    String imageURL1 = "http://quesdesk.hostzi.com/Sunset.jpg";
+    String imageURL2 = "http://quesdesk.hostzi.com/abc.bmp";
+
+    ImageLoader imageLoader;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        receivedJSON = (TextView)findViewById(R.id.received_json);
+        receivedJSON = (TextView) findViewById(R.id.received_json);
+        imageView = (ImageView) findViewById(R.id.image);
+        networkImageView = (NetworkImageView) findViewById(R.id.image_network);
 
         progressDialog = new ProgressDialog(this);
         progressDialog.setMessage("Loading...");
         progressDialog.show();
 
-        Map<String, String> params = new HashMap<>();
+        /*Map<String, String> params = new HashMap<>();
         params.put("name", "prateek1");
-        params.put("email", "prateek1@gmail.com");
+        params.put("email", "prateek1@gmail.com");*/
 
 
         /*CustomObjectRequest jsonObjectRequest = new CustomObjectRequest(Request.Method.POST, URL, params, new Response.Listener<JSONObject>(){
@@ -65,7 +71,7 @@ public class MainActivity extends Activity {
         });
         AppController.getInstance().addToRequestQueue(jsonObjectRequest);*/
 
-        CustomArrayRequest jsonArrayRequest = new CustomArrayRequest(Request.Method.POST, URL, params, new Response.Listener<JSONArray>(){
+        /*CustomArrayRequest jsonArrayRequest = new CustomArrayRequest(Request.Method.POST, URL, params, new Response.Listener<JSONArray>(){
             @Override
             public void onResponse(JSONArray jsonArray) {
                 text = jsonArray.toString();
@@ -80,7 +86,33 @@ public class MainActivity extends Activity {
                 progressDialog.hide();
             }
         });
-        AppController.getInstance().addToRequestQueue(jsonArrayRequest);
+        AppController.getInstance().addToRequestQueue(jsonArrayRequest);*/
+
+        /*ImageRequest imageRequest = new ImageRequest(imageURL1, new Response.Listener<Bitmap>() {
+            @Override
+            public void onResponse(Bitmap bitmap) {
+                imageView.setImageBitmap(bitmap);
+                progressDialog.dismiss();
+            }
+        }, 0, 0, null, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError volleyError) {
+                        receivedJSON.setText("Error");
+                        progressDialog.dismiss();
+                    }
+                });
+        AppController.getInstance().addToRequestQueue(imageRequest);*/
+
+        /*imageLoader = AppController.getInstance().getImageLoader();
+        imageLoader.get(imageURL1,ImageLoader.getImageListener(imageView,R.drawable.def_image,R.drawable.err_image));
+
+        progressDialog.dismiss();*/
+
+        /*imageLoader = AppController.getInstance().getImageLoader();
+        networkImageView.setImageUrl(imageURL1, imageLoader);
+        progressDialog.dismiss();*/
+
+
     }
 
 }
